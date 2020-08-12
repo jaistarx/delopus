05a1a45501f5d70ff948d245a5e48f467f20f748
jaistarx <jaistarx.in@gmail.com>
2020-08-12 17:05:53 +0530

    updated cards

--- src/Cardscroll.js
+++ src/Cardscroll.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cardscroll.css";
 import Image from "react-bootstrap/Image";
 import ScrollingHorizontally from "./Horiscroll";
@@ -6,6 +6,17 @@ import { BsFillCaretDownFill } from "react-icons/bs";
 import { IconContext } from "react-icons/lib";
 
 function Scroll() {
+  const [drop, setDrop] = useState(0);
+  const faq = (e) => {
+    // if(e===1){
+    //   setDrop(1)
+    // }
+    // else if(e===2){
+    //   setDrop(2)
+    // }
+    if (e === drop) setDrop(0);
+    else setDrop(e);
+  };
   return (
     <div style={{ backgroundColor: "black" }}>
       <Image
@@ -13,30 +24,100 @@ function Scroll() {
         fluid
         style={{ marginLeft: "20%", marginRight: "20%", width: "60%" }}
       />
-      <ScrollingHorizontally></ScrollingHorizontally>
+      {/* <ScrollingHorizontally></ScrollingHorizontally> */}
       {/* <img src="images/tcr1.png" alt="teacher" style={{width:'30%', height:'30%'}}/> */}
       <div>
         <div className="flexing-props">
-          <div>
-            <p> gftujfjgltflu/</p>
+          <div style={{ paddingTop: "1.5%" }}>
+            <p>
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
           </div>
-          <div>
+          <div style={{ paddingTop: "1.5%" }} onClick={(e) => faq(1)}>
             <IconContext.Provider
               value={{
-                // size: "30.5%",
-                color: "orange"
+                // size: "25%",
+                color: "orange",
               }}
             >
               <BsFillCaretDownFill />
             </IconContext.Provider>
           </div>
         </div>
-        <div>
-          <p> gftujfjgltflu/</p>
+        {drop === 1 && (
+          <div className="dropping">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry.Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry.Lorem Ipsum is simply dummy text
+              of the printing and typesetting industry.
+            </p>
+          </div>
+        )}
+
+        <div className="flexing-props">
+          <div style={{ paddingTop: "1.5%" }}>
+            <p>
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div style={{ paddingTop: "1.5%" }} onClick={(e) => faq(2)}>
+            <IconContext.Provider
+              value={{
+                // size: "30.5%",
+                color: "orange",
+              }}
+            >
+              <BsFillCaretDownFill />
+            </IconContext.Provider>
+          </div>
         </div>
-        <div>
-          <p> gftujfjgltflu/</p>
+        {drop === 2 && (
+          <div className="dropping">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry.Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry.Lorem Ipsum is simply dummy text
+              of the printing and typesetting industry.
+            </p>
+          </div>
+        )}
+        <div className="flexing-props">
+          <div style={{ paddingTop: "1.5%" }}>
+            <p>
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div style={{ paddingTop: "1.5%" }} onClick={(e) => faq(3)}>
+            <IconContext.Provider
+              value={{
+                // size: "30.5%",
+                color: "orange",
+              }}
+            >
+              <BsFillCaretDownFill />
+            </IconContext.Provider>
+          </div>
         </div>
+        {drop === 3 && (
+          <div className="dropping">
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry.Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry.Lorem Ipsum is simply dummy text
+              of the printing and typesetting industry.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/Horiscroll.js
+++ src/Horiscroll.js
@@ -1,20 +1,111 @@
-import React, { Component } from 'react'
-import HorizontalScroll from 'react-scroll-horizontal'
- 
-class ScrollingHorizontally extends Component {
-  render() {
-    const child   = { width: `30em`, height: `100%`,color:'black'}
-    const parent  = { width: `60em`, height: `100%`,color:'black'}
-    return (
-      <div style={parent}>
-        <HorizontalScroll>
-            <div style={child} ><p>IMAGE 1</p></div>
-            <div style={child} ><p>IMAGE 1</p></div>
-            <div style={child} ><p>IMAGE 1</p></div>
-        </HorizontalScroll>
-      </div>
-    )
-  }
-}
+// import React from "react";
+// import ImageScroller from "react-image-scroller";
+// import "./Horiscroll.css";
 
-export default ScrollingHorizontally;
+// export default function ScrollingHorizontally() {
+//   return (
+//     <div>
+//       <ImageScroller scrollWithArrows="true">
+//         <img src="images/tcr1.png" style={{width:"30%"}} alt="First" />
+//         <img src="images/tcr1.png" style={{width:"30%"}} alt="Second" />
+//         <img src="images/tcr1.png" style={{width:"30%"}} alt="Third" />
+//         <img src="images/tcr1.png" style={{width:"30%"}} alt="Fourth" />
+//         <img src="images/tcr1.png" style={{width:"30%"}} alt="Fifth" />
+//       </ImageScroller>
+//     </div>
+//   );
+// }
+
+// import React from "react";
+// import AliceCarousel from "react-alice-carousel";
+// import "react-alice-carousel/lib/alice-carousel.css";
+// import "./Horiscroll.css"
+
+// export default function ScrollingHorizontally() {
+//   const handleOnDragStart = (e) => e.preventDefault();
+//   return (
+//     <div>
+//       <AliceCarousel mouseTrackingEnabled responsive={2}>
+//         <img
+//           src="images/tcr1.png"
+//           style={{width:"30%"}}
+//           onDragStart={handleOnDragStart}
+//           className="yours-custom-class"
+//         />
+//         <img
+//           src="images/tcr1.png"
+//           onDragStart={handleOnDragStart}
+//           className="yours-custom-class"
+//         />
+//         <img
+//           src="images/tcr1.png"
+//           onDragStart={handleOnDragStart}
+//           className="yours-custom-class"
+//         />
+//         <img
+//           src="images/tcr1.png"
+//           onDragStart={handleOnDragStart}
+//           className="yours-custom-class"
+//         />
+//         <img
+//           src="images/tcr1.png"
+//           onDragStart={handleOnDragStart}
+//           className="yours-custom-class"
+//         />
+//       </AliceCarousel>
+//     </div>
+//   );
+// }
+
+// import React from "react";
+// import OwlCarousel from "react-owl-carousel";
+// import "owl.carousel/dist/assets/owl.carousel.css";
+// import "owl.carousel/dist/assets/owl.theme.default.css";
+
+// // ....
+
+// // className "owl-theme" is optional
+// export default function ScrollingHorizontally() {
+//   return (
+//     <div>
+//       <OwlCarousel className="owl-theme" loop margin={10} nav>
+//         <div class="item">
+//           <h4>1</h4>
+//         </div>
+//         <div class="item">
+//           <h4>2</h4>
+//         </div>
+//         <div class="item">
+//           <h4>3</h4>
+//         </div>
+//         <div class="item">
+//           <h4>4</h4>
+//         </div>
+//         <div class="item">
+//           <h4>5</h4>
+//         </div>
+//         <div class="item">
+//           <h4>6</h4>
+//         </div>
+//         <div class="item">
+//           <h4>7</h4>
+//         </div>
+//         <div class="item">
+//           <h4>8</h4>
+//         </div>
+//         <div class="item">
+//           <h4>9</h4>
+//         </div>
+//         <div class="item">
+//           <h4>10</h4>
+//         </div>
+//         <div class="item">
+//           <h4>11</h4>
+//         </div>
+//         <div class="item">
+//           <h4>12</h4>
+//         </div>
+//       </OwlCarousel>
+//     </div>
+//   );
+// }
